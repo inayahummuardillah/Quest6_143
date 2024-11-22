cd951b76c6dcc9bd27f19bf06b759a8ab101b855
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2024-11-22 16:10:29 +0630

    Di mahasiswa form view ini juga menggunakan Out lined text field untuk menyusun elemen secara horizontal, semua elemen dari row akan ditempatkan dalam baris, dari kiri ke kanan. Dan menggunakan Button yang berupa on back button clicked yang berufungsi untuk kembali ke layar sebelumnya, dan on submit button clicked untuk memanggil dengan data tersebut

--- app/src/main/java/com/example/p6/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/p6/ui/view/screen/MahasiswaFormView.kt
@@ -86,6 +86,98 @@ fun MahasiswaFormView(
                 )
             }
         }
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    ))
+                .fillMaxSize(),
+
+            ){
+            Column(
+                modifier = Modifier.fillMaxSize().padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Masukan Data Kamu",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp)
+                Text(text = "Isi sesuai data yang kamu daftarkan",
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = nama,
+                    onValueChange = { nama = it},
+                    label = { Text(text = "Nama Mahasiswa") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp)
+                )
+
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = nim,
+                    onValueChange = { nama = it},
+                    label = { Text(text = "Nomor Induk Mahasiswa") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp
+                    )
+                )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = email,
+                    onValueChange = { nama = it},
+                    label = { Text(text = "Email Mahasiswa") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.Email,
+                            contentDescription = ""
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Done
+                    ),
+                    singleLine = true,
+                    shape = RoundedCornerShape(50.dp)
+                    )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){
+                    Button(onClick =  { onBackButtonClicked() }) {
+                        Text(text = "Kembali")
+                    }
+                    Button(onClick =  { onSubmitButtonClicked(listData) }) {
+                        Text(text = "Simpan")
+                    }
+                }
+            }
         }
     }
 }
